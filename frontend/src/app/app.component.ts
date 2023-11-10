8dd389fd110f4369023a98a8edb9487dfb4ae28c
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-11-10 16:24:13 +0100

    Add header component with logo, search bar, and user icon

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './shared/ui/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
