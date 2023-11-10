9dad48b48a49f38cb28798eda231db50afb35ba9
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-11-10 17:30:08 +0100

    Update app component to use router-outlet.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './shared/ui/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'proximity';
